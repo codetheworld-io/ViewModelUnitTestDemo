f6427a3093a9d00e2cfe06c65be58ca834cd6832
Hoang Dinh <hoang.dv@outlook.com>
2025-04-26 22:52:11 +0900

    Add Login UI test by UI Automator

--- app/src/androidTest/java/io/codetheworld/viewmodelunittestdemo/LoginTest.kt
+++ app/src/androidTest/java/io/codetheworld/viewmodelunittestdemo/LoginTest.kt
@@ -10,18 +10,33 @@ import androidx.test.espresso.matcher.ViewMatchers.withHint
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import io.codetheworld.viewmodelunittestdemo.EspressoExtensions.Companion.waitForViewVisible
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
+private const val TIMEOUT = 20000L
 
 @RunWith(AndroidJUnit4::class)
 class LoginTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(LoginActivity::class.java)
 
+    private lateinit var device: UiDevice
+
+    @Before
+    fun setUp() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    }
+
     @Test
     fun shouldHandleWrongUsernameOrPassword() {
+        val errorMessage = "Username or password is not correct!"
         onView(withHint("Username")).perform(
             click(),
             typeText("wrong value"),
@@ -31,7 +46,8 @@ class LoginTest {
             typeText("admin"),
         )
         onView(withText("Login")).perform(click())
-        waitForViewVisible(withText("Username or password is not correct!")).check(matches(isDisplayed()))
+        device.wait(Until.hasObject(By.text(errorMessage)), TIMEOUT)
+        onView(withText(errorMessage)).check(matches(isDisplayed()))
 
         onView(withHint("Username")).perform(
             click(),
@@ -44,7 +60,12 @@ class LoginTest {
             typeText("wrong value"),
         )
         onView(withText("Login")).perform(click())
-        waitForViewVisible(withText("Username or password is not correct!")).check(matches(isDisplayed()))
+        device.wait(Until.hasObject(By.text(errorMessage)), TIMEOUT)
+        waitForViewVisible(withText(errorMessage)).check(
+            matches(
+                isDisplayed()
+            )
+        )
     }
 
     @Test
@@ -59,6 +80,7 @@ class LoginTest {
         )
         onView(withText("Login")).perform(click())
 
-        waitForViewVisible(withText("Hello World!")).check(matches(isDisplayed()))
+        device.wait(Until.hasObject(By.text("Hello World!")), TIMEOUT)
+        onView(withText("Hello World!")).check(matches(isDisplayed()))
     }
 }
